d5bc9aaff638dc25b21f1d60944b240bead60c47
LisaC1234 <lisa.chabrier@outlook.fr>
2020-07-16 15:36:55 +0300

    analyse over human result

--- Code/human_comparison.py
+++ Code/human_comparison.py
@@ -1,9 +1,24 @@
 #! /usr/bin/env python3
 # coding: utf-8
 import argparse
-import parseur_Diffuse
+from subprocess import Popen, PIPE
 
+def read_fasta(list_uniprot):
+	res = {}
+	l = list(range(1,23)) + ['x','y']
+	for i in l:
+		res[i] = []
+	for gene in list_uniprot:
+		for numb in l:
+			cmd = ["grep", "-c", gene, "Data/Human_by_chromosome/chromosome_"+str(numb)+".fasta"]
+			p = Popen(cmd, stdout=PIPE, stderr=PIPE)
+			stdout, stderr = p.communicate()
+			if "1" in str(stdout):
+				res[numb].append(gene)
+				break
+	return res
 
+		
 
 def extract_composites(path): # Return type : {composite_gene_ID : [[(family,ID of the hit), ...], ...]} and each sub-list is a domain
 	res = {}
@@ -127,39 +142,40 @@ def main():
 	all_vs_all = list_of_uniprot_composites(dico_composites, dico_names)
 	#print(len(one_by_one), len(all_vs_all))
 	intersect = [c for c in one_by_one if c in all_vs_all]
-	#print(intersect == one_by_one) # should be true, we want all the fusion event occuring in only one chromosome to appear in an all_vs_all chromosome analysis.
-	#print(len(intersect))
-	#print(intersect)
-	#print('\n\n\n')
-	#print([c for c in one_by_one if c not in all_vs_all])
 	inv_dico_names = {v:k for k,v in dico_names.items()}
 	list_nb_genes = [1975 , 1249 , 1032 , 730 , 842 , 970 , 927 , 643 , 741 , 708 , 1258 , 986 , 310 , 698 , 559 , 794 , 1121 , 264 , 1372 , 525 , 221 , 464 , 790 , 37]
-	
+	dico_comp_by_genes = read_fasta(all_vs_all)
 	
 	result_file = "Docs/tableau_all_vs_all.md"
 	with open(result_file, "w") as res:
-		res.write("| Chromosome | Genes | Composites | Components (uniques) | Families of composites |\n")
-		res.write("|------------|:-----:|:----------:|:--------------------:|:----------------------:|\n")
+		res.write("| Chromosome | Genes | Composites | Composites also found with one by one | Components (uniques) | Families of composites |\n")
+		res.write("|------------|:-----:|:----------:|:--------------------:|:--------------------:|:----------------------:|\n")
 		l = list(range(24))
 		for i in l:
 			if i+1 == 23:
 				name = "chromosome_" + "x"
+				numb = "x"
 			elif i+1 == 24 :
 				name = "chromosome_" + "y"
+				numb = "y"
 			else :
 				name = "chromosome_" + str(i+1)
-				
+				numb = i+1
+			
+			print(name)	
 			
 			list_composite = [c for c in by_chromosome[i] if c in all_vs_all]
-			nb_composite = len(list_composite)
+			nb_composite_both = len(list_composite)
+			
 			#print("ici \n", list_composite)
 			
 			comp, unique_comp = nb_components(list_composite, dico_composites, inv_dico_names)
 			
 			nb_families = composites_families(list_composite, inv_dico_names)
 			
+			nb_composite =  len(dico_comp_by_genes[numb])
 			
-			res.write("| " + name + " | " + str(list_nb_genes[i]) + " | " + str(nb_composite) + " | " + str(comp) + "(" + str(unique_comp) + ")" + " | " + str(nb_families) + " | \n" )
+			res.write("| " + name + " | " + str(list_nb_genes[i]) + " | " +str(nb_composite) + " | " + str(nb_composite_both) + " | " + str(comp) + "(" + str(unique_comp) + ")" + " | " + str(nb_families) + " | \n" )
 		
 		
 if __name__ == "__main__":
--- Docs/tableau_all_vs_all.md
+++ Docs/tableau_all_vs_all.md
@@ -1,26 +1,26 @@
-| Chromosome | Genes | Composites | Components (uniques) | Families of composites |
-|------------|:-----:|:----------:|:--------------------:|:----------------------:|
-| chromosome_1 | 1975 | 3 | 22(22) | 3 | 
-| chromosome_2 | 1249 | 8 | 24(14) | 3 | 
-| chromosome_3 | 1032 | 3 | 66(61) | 3 | 
-| chromosome_4 | 730 | 0 | 0(0) | 0 | 
-| chromosome_5 | 842 | 0 | 0(0) | 0 | 
-| chromosome_6 | 970 | 9 | 55(27) | 4 | 
-| chromosome_7 | 927 | 6 | 123(82) | 5 | 
-| chromosome_8 | 643 | 2 | 52(45) | 2 | 
-| chromosome_9 | 741 | 0 | 0(0) | 0 | 
-| chromosome_10 | 708 | 2 | 28(28) | 2 | 
-| chromosome_11 | 1258 | 1 | 4(4) | 1 | 
-| chromosome_12 | 986 | 0 | 0(0) | 0 | 
-| chromosome_13 | 310 | 0 | 0(0) | 0 | 
-| chromosome_14 | 698 | 0 | 0(0) | 0 | 
-| chromosome_15 | 559 | 15 | 143(25) | 3 | 
-| chromosome_16 | 794 | 12 | 127(93) | 11 | 
-| chromosome_17 | 1121 | 3 | 89(88) | 3 | 
-| chromosome_18 | 264 | 0 | 0(0) | 0 | 
-| chromosome_19 | 1372 | 105 | 2952(357) | 30 | 
-| chromosome_20 | 525 | 0 | 0(0) | 0 | 
-| chromosome_21 | 221 | 0 | 0(0) | 0 | 
-| chromosome_22 | 464 | 2 | 8(8) | 2 | 
-| chromosome_x | 790 | 3 | 46(36) | 2 | 
-| chromosome_y | 37 | 0 | 0(0) | 0 | 
+| Chromosome | Genes | Composites | Composites also found with one by one | Components (uniques) | Families of composites |
+|------------|:-----:|:----------:|:--------------------:|:--------------------:|:----------------------:|
+| chromosome_1 | 1975 | 22 | 3 | 22(22) | 3 | 
+| chromosome_2 | 1249 | 28 | 8 | 24(14) | 3 | 
+| chromosome_3 | 1032 | 12 | 3 | 66(61) | 3 | 
+| chromosome_4 | 730 | 5 | 0 | 0(0) | 0 | 
+| chromosome_5 | 842 | 8 | 0 | 0(0) | 0 | 
+| chromosome_6 | 970 | 21 | 9 | 55(27) | 4 | 
+| chromosome_7 | 927 | 32 | 6 | 123(82) | 5 | 
+| chromosome_8 | 643 | 18 | 2 | 52(45) | 2 | 
+| chromosome_9 | 741 | 19 | 0 | 0(0) | 0 | 
+| chromosome_10 | 708 | 19 | 2 | 28(28) | 2 | 
+| chromosome_11 | 1258 | 12 | 1 | 4(4) | 1 | 
+| chromosome_12 | 986 | 12 | 0 | 0(0) | 0 | 
+| chromosome_13 | 310 | 3 | 0 | 0(0) | 0 | 
+| chromosome_14 | 698 | 4 | 0 | 0(0) | 0 | 
+| chromosome_15 | 559 | 22 | 15 | 143(25) | 3 | 
+| chromosome_16 | 794 | 23 | 12 | 127(93) | 11 | 
+| chromosome_17 | 1121 | 13 | 3 | 89(88) | 3 | 
+| chromosome_18 | 264 | 8 | 0 | 0(0) | 0 | 
+| chromosome_19 | 1372 | 156 | 105 | 2952(357) | 30 | 
+| chromosome_20 | 525 | 6 | 0 | 0(0) | 0 | 
+| chromosome_21 | 221 | 1 | 0 | 0(0) | 0 | 
+| chromosome_22 | 464 | 6 | 2 | 8(8) | 2 | 
+| chromosome_x | 790 | 15 | 3 | 46(36) | 2 | 
+| chromosome_y | 37 | 2 | 0 | 0(0) | 0 | 
